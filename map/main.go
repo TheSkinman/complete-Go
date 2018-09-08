01a00d17820ba44faa146083cf432b0310e9109f
Norman Skinner <Norman.Skinner@TalkingBeltGames.com>
2018-09-08 07:52:46 -0700

    Update

---
+++ map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":    "#ff0000",
+		"green":  "#008000",
+		"blue":   "#0000ff",
+		"yellow": "#ffff00",
+		"lime":   "#00ff00",
+	}
+
+	fmt.Println(colors)
+}
